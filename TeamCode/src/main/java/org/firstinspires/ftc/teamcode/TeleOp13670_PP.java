cfd79bf221ef23e8dd0ca9aad1947754e92f50a4
Jontiveros91 <117313084+Jontiveros91@users.noreply.github.com>
2023-01-23 10:38:18 -0600

    Update TeleOp13670_PP.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp13670_PP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp13670_PP.java
@@ -4,67 +4,63 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
 //@Disabled
-@TeleOp(name = "TeleOp13670_PP", group = "Ontiveros")
+@TeleOp(name="Teleop 3", group="Ontiveros")
+//@Disabled
 public class TeleOp13670_PP extends LinearOpMode {
 
-    Hardware13670_4 robot = new Hardware13670_4();
-    private final ElapsedTime runtime = new ElapsedTime();
-    static final double FORWARD_SPEED = 0.6;
-    double          armOffset = 0;                // Servo mid position
-    final double    ARM_SPEED  = 0.001 ;
-    // sets rate to move servo
-    // Test
-    //double flipOffset = 0.5;
-    
+    /* Declare OpMode members. */
+    Hardware13670_4 robot  = new Hardware13670_4();   // Use the updated Mecanum's hardware
+
+    double howfast;
+    double shift;
+    double turn;
+
     @Override
     public void runOpMode() {
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
         robot.init(hardwareMap, telemetry);
-        Hardware13670_4.noEncoder();
 
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Driver");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
         waitForStart();
+
+        // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+            // In this mode the Left stick moves the robot fwd and back, the Right stick turns left and right.
+            // This way it's also easy to just drive straight, or just turn.
+            howfast = gamepad1.left_stick_y;
+            shift = gamepad1.left_stick_x;
+            turn = gamepad1.right_stick_x;
 
-            // use gamepad1 left stick to go forward, backward, and turn
-            if (gamepad1.left_stick_button) {
-                Hardware13670_4.leftFront.setPower(-1* (gamepad1.left_stick_y) + 1 * (gamepad1.left_stick_x));
-                Hardware13670_4.rightFront.setPower(-1 * (gamepad1.left_stick_y) - 1 * (gamepad1.left_stick_x));
-                Hardware13670_4.leftBack.setPower(-1 * (gamepad1.left_stick_y) + 1 * (gamepad1.left_stick_x));
-                Hardware13670_4.rightBack.setPower(-1 * (gamepad1.left_stick_y) - 1 * (gamepad1.left_stick_x));
-            }
-            else {
-                Hardware13670_4.leftFront.setPower(-0.7 * (gamepad1.left_stick_y) + 0.7 * (gamepad1.left_stick_x));
-                Hardware13670_4.rightFront.setPower(-0.7 * (gamepad1.left_stick_y) - 0.7 * gamepad1.left_stick_x);
-                Hardware13670_4.leftBack.setPower(-0.7 * (gamepad1.left_stick_y) + 0.7 * gamepad1.left_stick_x);
-                Hardware13670_4.rightBack.setPower(-0.7 * (gamepad1.left_stick_y) - 0.7 * (gamepad1.left_stick_x));
-            }
+            Hardware13670_4.leftFront.setPower(gamepad1.left_stick_y - gamepad1.right_stick_x);
+            Hardware13670_4.rightFront.setPower(gamepad1.left_stick_y + gamepad1.right_stick_x);
+            Hardware13670_4.leftBack.setPower(gamepad1.left_stick_y - gamepad1.right_stick_x);
+            Hardware13670_4.rightBack.setPower(gamepad1.left_stick_y + gamepad1.right_stick_x);
 
-            telemetry.addData("leftFront position", Hardware13670_4.leftFront.getCurrentPosition());
-            telemetry.update();
+            if (gamepad1.left_stick_x > 0)
+                robot.goRight(shift);
+            else if (gamepad1.left_stick_x < 0)
+                robot.goRight(shift);
+            else if (gamepad1.left_stick_x == 0)
+                robot.goRight(0);
 
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("Motors",gamepad1.left_stick_y +", " + gamepad1.left_stick_x);
-            telemetry.update();
-
-            //use gamepad1 right stick to move left and right
-            if(gamepad1.right_stick_x> 0){
-                robot.goRight(gamepad1.right_stick_x);
-            }
-            if(gamepad1.right_stick_x< 0) {
-                robot.goLeft(-gamepad1.right_stick_x);
-            }
-            // use right & left trigger to move the lift up & down
-            if (gamepad1.left_trigger > 0 ) {
-                robot.lift.setPower(gamepad1.left_trigger);
-            }
-            else if (gamepad1.right_trigger > 0) {
-                robot.lift.setPower(-gamepad1.right_trigger);
-            }
-            else {
+            //use trigger to lift
+            if (gamepad1.left_trigger > 0)
+                robot.lift.setPower(gamepad1.left_trigger); // lift up
+            else if (gamepad1.right_trigger > 0)
+                robot.lift.setPower(-1 * gamepad1.right_trigger); // lift down
+            else
                 robot.lift.setPower(0);
-            }
-            // use right & left bumper to open/close claw
+
+           // use right & left bumper to open/close claw
             if (gamepad1.right_bumper){
                 robot.claw.setPower(1);
             }
@@ -72,8 +68,16 @@ public class TeleOp13670_PP extends LinearOpMode {
                 robot.claw.setPower(-1);
             }
             else {
-                robot.claw.setPower(0);
+                robot.claw.setPower(1);
             }
+            if(gamepad1.a){
+                robot.claw.setPower(-1);
+            }
+            // Send telemetry message to signify robot running;
+            // telemetry.addData("claw",  "Offset = %.2f", clawOffset);
+            // telemetry.addData("left",  "%.2f", left);
+            //telemetry.addData("right", "%.2f", right);
+            //telemetry.update();
 
         }
 
